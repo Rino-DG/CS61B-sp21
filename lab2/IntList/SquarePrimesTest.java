464e08a3149dcbfd4a4285142286a149f09c2023
Rino De Guzman <rinodeguz@gmail.com>
2022-07-05 10:06:45 -0700

    Before changing squarePrimes method

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,12 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquareAllPrimes() {
+        IntList lst = IntList.of(2, 3, 5, 11, 17);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 25 -> 121 -> 289", lst.toString());
+        assertFalse(changed);
+    }
 }
